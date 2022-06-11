0b8efa5aaa5d4c8e27547bb226f670d7937798b5
Mykolas Raudžius <raudziusmykolas@gmail.com>
2022-06-11 12:48:47 +0300

    feat: output movie info property with its key

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -14,7 +14,16 @@ function renderMovies() {
   movieList.innerHTML = '';
   movies.forEach(movie => {
     const movieEl = document.createElement('li');
-    movieEl.textContent = movie.info.title;
+
+    let text = movie.info.title + ' - ';
+    
+    for (const key in movie.info) {
+      if (key !== 'title') {
+        text += `${key}: ${movie.info[key]}`;
+      }
+    }
+
+    movieEl.textContent = text;
     movieList.append(movieEl);
   });
 }
